937e4ff0403b22b70506dc637adcbf16ff87af76
AhmedMohamed5536 <ahmed.mohamed43310@gmail.com>
2025-07-14 10:50:15 +0300

    Add project files.

---
+++ CSSSAssigment 004/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CSSSAssigment_004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
