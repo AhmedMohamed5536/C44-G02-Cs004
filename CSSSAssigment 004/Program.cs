358521adbf806207413f448328963f35e046a1a0
AhmedMohamed5536 <ahmed.mohamed43310@gmail.com>
2025-07-14 12:31:51 +0300

    Q14

--- CSSSAssigment 004/Program.cs
+++ CSSSAssigment 004/Program.cs
@@ -381,7 +381,7 @@ class Program
         Console.WriteLine("Max = " + max);
     }
 }
-*/
+
 
 // 13- Write a program in C# Sharp to find the second largest element in an array.
 
@@ -413,5 +413,45 @@ class Program
     }
 }
 
+*/
 
+// 14- Consider an Array of Integer values with size N, having values as
+//in this Example
+//15- 7 0 0 0 5 6 7 5 0 7 5 3
+//write a program to find the longest distance between Two equal cells. In this example. The distance is measured by the number Of cells- for example, the distance between the first and the fourth cell is 2 (cell 2 and cell 3).
+//In the example above, the longest distance is between the first 7 and the
+//10th 7, with a distance of 8 cells, i.e. the number of cells between the 1st
+//And the 10th 7s.
+//Note:
+//Array values will be taken from the user
+//If you have input like 1111111 then the distance is the number of
+//Cells between the first and the last cell.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 7, 0, 0, 0, 5, 6, 7, 5, 0, 7, 5, 3 };
+
+        int maxDistance = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = arr.Length - 1; j > i; j--)
+            {
+                if (arr[i] == arr[j])
+                {
+                    int distance = j - i;
+                    if (distance > maxDistance)
+                        maxDistance = distance;
+                    break; 
+                }
+            }
+        }
+
+        Console.WriteLine("Longest distance = " + maxDistance);
+    }
+}
 
